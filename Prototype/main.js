81741d823ee7066c7a540afbb40c0c27f5459dfd
KamalveerStarkyDev <kamalveer356@gmail.com>
2024-09-04 21:45:18 +0530

    version 1.3 push

---
+++ Prototype/main.js
@@ -0,0 +1,40 @@
+// to top function implementation
+const toTop = document.querySelector("#go-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 720) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
+// dark theme toggle implementation
+const themeIcon = document.querySelector("#dark-theme i");
+const currentTheme = localStorage.getItem("theme") || "light";
+
+if (currentTheme === "dark") {
+  document.body.classList.add("dark");
+  themeIcon.classList.add("ri-moon-line");
+} else {
+  themeIcon.classList.add("ri-sun-line");
+}
+
+themeIcon.parentElement.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Toggle icons based on the current theme
+  if (document.body.classList.contains("dark")) {
+    themeIcon.classList.remove("ri-sun-line");
+    themeIcon.classList.add("ri-moon-line");
+    document.querySelector(".logo-image").src =
+      "./assets/images/icons/icon-white.svg";
+    localStorage.setItem("theme", "dark");
+  } else {
+    themeIcon.classList.remove("ri-moon-line");
+    themeIcon.classList.add("ri-sun-line");
+    document.querySelector(".logo-image").src =
+      "./assets/images/icons/icon-black.svg";
+    localStorage.setItem("theme", "light");
+  }
+});
